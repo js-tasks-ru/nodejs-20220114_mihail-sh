aba3e02880d5a77ca059eebbc5ce75365fe3829a
MikhailShakhanov <mikhail.shakhanov@gmail.com>
2022-02-02 09:11:00 -0800

    add solution for 05-koajs: 01-chat-app

--- 05-koajs/01-chat-app/app.js
+++ 05-koajs/01-chat-app/app.js
@@ -1,17 +1,31 @@
+//npm test -- --grep "koajs/chat-app"
 const path = require('path');
 const Koa = require('koa');
 const app = new Koa();
+const { EventEmitter } = require('events');
 
 app.use(require('koa-static')(path.join(__dirname, 'public')));
 app.use(require('koa-bodyparser')());
 
 const Router = require('koa-router');
 const router = new Router();
+const chatEvent = new EventEmitter();
 
 router.get('/subscribe', async (ctx, next) => {
+  await new Promise((resolve) => chatEvent.once('message', (message) => {
+    ctx.body = message;
+    resolve(true);
+  }));
 });
 
 router.post('/publish', async (ctx, next) => {
+  if (ctx.request.body.message) {
+    chatEvent.emit('message', ctx.request.body.message);
+    ctx.body = 'Publish';
+  } else {
+    ctx.body = 'Need message';
+    ctx.status = 400;
+  }
 });
 
 app.use(router.routes());
