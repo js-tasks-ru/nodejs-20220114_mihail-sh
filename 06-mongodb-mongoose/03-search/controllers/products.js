1d22641d5b5d8eca6bae758289205b900a5a98fb
MikhailShakhanov <mikhail.shakhanov@gmail.com>
2022-02-06 11:41:57 -0800

    add solution for 06-mongodb-mongoose: 01-schema-model 02-rest-api 03-search

--- 06-mongodb-mongoose/03-search/controllers/products.js
+++ 06-mongodb-mongoose/03-search/controllers/products.js
@@ -1,3 +1,12 @@
+const Product = require('../models/Product');
 module.exports.productsByQuery = async function productsByQuery(ctx, next) {
-  ctx.body = {products: []};
+  const { query } = ctx.query;
+
+  if (!query) {
+    const products = await Product.find();
+    ctx.body = { products: products };
+  };
+  const products = await Product.find({ $text: { $search: query } });
+
+  ctx.body = { products: products };
 };
