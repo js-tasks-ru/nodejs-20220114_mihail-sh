06d6eaf6a68de63e8355c0835e727ed8444943fe
mi.shakhanov <mi.shakhanov@jet.su>
2022-02-20 18:58:33 +0300

    add solution for 08-email: 01-registration 02-order

--- 08-email/01-registration/controllers/registration.js
+++ 08-email/01-registration/controllers/registration.js
@@ -3,9 +3,43 @@ const User = require('../models/User');
 const sendMail = require('../libs/sendMail');
 
 module.exports.register = async (ctx, next) => {
+  const { email, displayName, password } = ctx.request.body;
+  const userToFind = await User.findOne({ email });
 
+  if (userToFind) {
+    ctx.status = 400;
+    ctx.body = { errors: { email: 'Такой email уже существует' } };
+    return;
+  }
+
+  const verificationToken = uuid();
+  const u = new User({ email, displayName, verificationToken });
+  await u.setPassword(password);
+  await u.save();
+  await sendMail({
+    template: 'confirmation',
+    locals: { token: verificationToken },
+    to: email,
+    subject: 'Подтвердите почту',
+  });
+
+  ctx.status = 200;
+  ctx.body = { status: 'ok' };
 };
 
 module.exports.confirm = async (ctx, next) => {
+  const { verificationToken } = ctx.request.body;
 
+  const userToFind = await User.findOne({ verificationToken: verificationToken });
+  if (!userToFind) {
+    ctx.status = 400;
+    ctx.body = { error: 'Ссылка подтверждения недействительна или устарела' };
+    return;
+  }
+
+  userToFind.verificationToken = undefined;
+  await userToFind.save();
+  const token = await ctx.login(userToFind);
+  ctx.status = 200;
+  ctx.body = { token: token };
 };
--- 08-email/02-order/controllers/orders.js
+++ 08-email/02-order/controllers/orders.js
@@ -1,8 +1,35 @@
 const Order = require('../models/Order');
+const Product = require('../models/Product');
 const sendMail = require('../libs/sendMail');
+const mapOrderConfirmation = require('../mappers/orderConfirmation');
+const mapOrder = require('../mappers/order');
 
 module.exports.checkout = async function checkout(ctx, next) {
+  const { product, phone, address } = ctx.request.body;
+
+  const productDoc = await Product.findById(product);
+
+  const order = new Order({
+    user: ctx.user,
+    product: productDoc,
+    phone: phone,
+    address: address,
+  });
+
+  await order.save();
+
+  await sendMail({
+    template: 'order-confirmation',
+    locals: mapOrderConfirmation(order, productDoc),
+    to: ctx.user.email,
+    subject: 'Подтверждение заказа',
+  });
+
+  ctx.status = 200;
+  ctx.body = { order: order._id };
 };
 
 module.exports.getOrdersList = async function ordersList(ctx, next) {
+  const orders = await Order.find({ user: ctx.user }).populate('product');
+  ctx.body = { orders: orders.map(mapOrder) };
 };
